80f40a83c7b0dd411781f58b830c0ff201488772
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 08:50:51 +0100

    Update and rename install-powwerbutton.sh to install-powerbutton.sh

---
+++ install-powerbutton.sh
@@ -0,0 +1,2 @@
+sudo mv poweroffbutton.py /usr/local/bin/
+sudo chmod +x /usr/local/bin/poweroffbutton.py
--- install-powwerbutton.sh
+++
@@ -1,2 +0,0 @@
-sudo mv powweroff.py /usr/local/bin/
-sudo chmod +x /usr/local/bin/powweroff.py

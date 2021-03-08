34c370c8bdb0cea9f0a06bcaa8d21a97f218c016
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 09:02:23 +0100

    update file name

--- install-powerbutton.sh
+++ install-powerbutton.sh
@@ -4,7 +4,7 @@ if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 sudo mv poweroffbutton.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/poweroffbutton.py
 
-sudo mv listen-for-shutdown.sh /etc/init.d/
-sudo chmod +x /etc/init.d/listen-for-shutdown.sh
-sudo update-rc.d listen-for-shutdown.sh defaults
-sudo /etc/init.d/listen-for-shutdown.sh start
+sudo mv poweroffbutton.sh /etc/init.d/
+sudo chmod +x /etc/init.d/poweroffbutton.sh
+sudo update-rc.d poweroffbutton.sh defaults
+sudo /etc/init.d/poweroffbutton.sh start

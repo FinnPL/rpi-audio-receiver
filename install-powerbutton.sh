5a1585676a5b522476f60622a49c876862c836ea
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 09:00:39 +0100

    auto start program on boot

--- install-powerbutton.sh
+++ install-powerbutton.sh
@@ -3,3 +3,8 @@ read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 sudo mv poweroffbutton.py /usr/local/bin/
 sudo chmod +x /usr/local/bin/poweroffbutton.py
+
+sudo mv listen-for-shutdown.sh /etc/init.d/
+sudo chmod +x /etc/init.d/listen-for-shutdown.sh
+sudo update-rc.d listen-for-shutdown.sh defaults
+sudo /etc/init.d/listen-for-shutdown.sh start

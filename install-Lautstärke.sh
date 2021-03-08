e4fca1c455fcebebb54b525d1f6c30ed5837acc1
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 09:20:24 +0100

    Update install-Lautstärke.sh

--- install-Lautstärke.sh
+++ install-Lautstärke.sh
@@ -5,3 +5,8 @@ if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 sudo apt install python3 idle3
 
 sudo apt-get install python-rpi.gpio python3-rpi.gpio
+
+sudo mv poweroffbutton.sh /etc/init.d/
+sudo chmod +x /etc/init.d/poweroffbutton.sh
+sudo update-rc.d poweroffbutton.sh defaults
+sudo /etc/init.d/poweroffbutton.sh start

833e841750f3b544ed429e40df0776b9f2f8772b
FinnLippok <77006988+FinnPL@users.noreply.github.com>
2021-03-08 09:12:53 +0100

    Update install-Lautstärke.sh

--- install-Lautstärke.sh
+++ install-Lautstärke.sh
@@ -1,1 +1,7 @@
+echo -n "Willst du Lautstärkeknöpfe einrichten? [y/N] "
+read REPLY
+if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+
+sudo apt install python3 idle3
+
 sudo apt-get install python-rpi.gpio python3-rpi.gpio
